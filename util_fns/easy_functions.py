44b74962dc9464270420e4d1d601b77702dc2c6f
Varun221 <yerram.varun@gmail.com>
2023-02-16 07:09:40 +0530

    quick changes

--- util_fns/easy_functions.py
+++ util_fns/easy_functions.py
@@ -5,9 +5,12 @@ import matplotlib.pyplot as plt
 def show_image(img, size=(8,8)):
     plt.figure(figsize=size)
     
+    while img.shape[0] == 1:
+        img = img[0]
+    
     if len(img.shape) == 3:
         plt.imshow(img[:, :, ::-1])
-    else:
+    elif (len(img.shape) == 2) or (img.shape[-1] == 1):
         plt.imshow(img, cmap='gray')
         
     plt.show()
