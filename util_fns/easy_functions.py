3ed173f933864094bfbbeaa197180bc98d26751a
Varun221 <yerram.varun@gmail.com>
2023-02-16 07:12:54 +0530

    quick changes

--- util_fns/easy_functions.py
+++ util_fns/easy_functions.py
@@ -8,9 +8,10 @@ def show_image(img, size=(8,8)):
     while img.shape[0] == 1:
         img = img[0]
     
-    if len(img.shape) == 3:
-        plt.imshow(img[:, :, ::-1])
-    elif (len(img.shape) == 2) or (img.shape[-1] == 1):
+
+    if (len(img.shape) == 2) or (img.shape[-1] == 1):
         plt.imshow(img, cmap='gray')
+    elif len(img.shape) == 3:
+        plt.imshow(img[:, :, ::-1])
         
     plt.show()
